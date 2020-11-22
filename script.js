d5afddc9ac212e7ed3842fde47d2e916694ecb38
Natalia <nasozontova@gmail.com>
2020-11-22 15:23:05 -0500

    added validations to script.js file

--- script.js
+++ script.js
@@ -20,57 +20,56 @@ function generatePassword() {
   //Declare empty String for the password options
   var passwordOptions = "";
 
+  //Declare empty String for the user password
+  var userPassword = "";
+
   //Declare variable for the password length
   var passwordLength = prompt("Please enter the length of your password");
   var passwordLength = parseInt(passwordLength);
-  console.log(typeof (passwordLength));
-  
-  //Check the conditions for the password lenght
-  if (passwordLength < 8) {
-    alert("The password's length should be at least 8 characters");
-    var passwordLength = prompt("Please enter the length of your password");
-    var passwordLength = parseInt(passwordLength);
-    console.log(typeof (passwordLength));
-  }
-  if (passwordLength > 128) {
-    alert("The password's length should be no be more than 128 characters");
-    var passwordLength = prompt("Please enter the length of your password");
-    var passwordLength = parseInt(passwordLength);
-    console.log(typeof (passwordLength));
-  }
 
-  //offer user to pick up from 4 characters options avalable 
-  var addUpperCase = confirm("Would you like to have Uppercase characters?")
-  if (addUpperCase === true) {
-    passwordOptions = passwordOptions + upperCase;
-    console.log(passwordOptions);
-  }
-  var addLowerCase = confirm("Would you like to have Lowercase characters?")
-  if (addLowerCase === true) {
-    passwordOptions = passwordOptions + lowerCase;
-    console.log(passwordOptions);
-  }
-  var addSpecialChar = confirm("Would you like to have Special characters?")
-  if (addSpecialChar === true) {
-    passwordOptions = passwordOptions + specChar;
-    console.log(passwordOptions);
-  }
-  var addNums = confirm("Would you like to have numeric characters?")
-  if (addNums === true) {
-    passwordOptions = passwordOptions + numeric;
-    console.log(passwordOptions);
-  }
+  //Validate the password length
+  if (passwordLength >= 8 && passwordLength <= 128) {
 
-  //Declare empty String for the user password
-  var userPassword = "";
-  
-  //Iterate from the options the user picked and generate unique password
-  for (var i = 0; i <= passwordLength; i++) {
-    var num = Math.floor((Math.random() * passwordOptions.length));
-    userPassword += passwordOptions.charAt(num);
+    //Promt for 4 character types to include in the password 
+    var addUpperCase = confirm("Would you like to have Uppercase characters?")
+    if (addUpperCase === true) {
+      passwordOptions += upperCase;
+      console.log(passwordOptions);
+    }
+    var addLowerCase = confirm("Would you like to have Lowercase characters?")
+    if (addLowerCase === true) {
+      passwordOptions += lowerCase;
+      console.log(passwordOptions);
+    }
+    var addSpecialChar = confirm("Would you like to have Special characters?")
+    if (addSpecialChar === true) {
+      passwordOptions += specChar;
+      console.log(passwordOptions);
+    }
+    var addNums = confirm("Would you like to have numeric characters?")
+    if (addNums === true) {
+      passwordOptions += numeric;
+      console.log(passwordOptions);
+    }
+
+    //Validate if at least one character type was selected
+    if (passwordOptions.length === 0) {
+      alert("At least one character type should be selected");
+      console.log(passwordOptions.length);
+    }
+
+    //Iterate from the options the user picked and generate unique password
+    for (var i = 0; i < passwordLength; i++) {
+      var num = Math.floor((Math.random() * passwordOptions.length));
+      userPassword += passwordOptions.charAt(num);
+    }
+    console.log(userPassword);
+    return userPassword;
+  } else {
+    alert("The password length must be at least 8 characters and no more than 128 characters");
+    return userPassword;
   }
-  console.log(userPassword);
-  return userPassword;
+  
 }
 
 // Write password to the #password input
